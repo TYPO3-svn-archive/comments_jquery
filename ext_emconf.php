a66be42b5cb1246960f6b8f62456ca2ceeeb2f46
stephenking <stephenking@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-11 11:05:46 +0000

    Changes after TER upload of 1.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/comments_jquery/trunk@28701 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "comments_jquery".
 #
-# Auto generated 11-01-2010 11:57
+# Auto generated 11-01-2010 12:04
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Makes EXT:comments use jQuery code, which is added to the end of <body>',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.0',
-	'dependencies' => '',
+	'version' => '1.0.1',
+	'dependencies' => 'comments',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -42,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"e0d2";s:10:"README.txt";s:4:"ee2d";s:27:"class.tx_commentsjquery.php";s:4:"8d50";s:12:"ext_icon.gif";s:4:"7edf";s:17:"ext_localconf.php";s:4:"8efe";s:14:"ext_tables.php";s:4:"b00a";s:19:"doc/wizard_form.dat";s:4:"5162";s:20:"doc/wizard_form.html";s:4:"195f";s:20:"res/jquery.cookie.js";s:4:"3847";s:32:"res/jquery.tx_comments_jquery.js";s:4:"0a65";s:16:"static/setup.txt";s:4:"e438";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"0ad8";s:27:"class.tx_commentsjquery.php";s:4:"8d50";s:12:"ext_icon.gif";s:4:"7edf";s:17:"ext_localconf.php";s:4:"8efe";s:14:"ext_tables.php";s:4:"b00a";s:12:"t3jquery.txt";s:4:"e0d2";s:19:"doc/wizard_form.dat";s:4:"5162";s:20:"doc/wizard_form.html";s:4:"195f";s:20:"res/jquery.cookie.js";s:4:"3847";s:32:"res/jquery.tx_comments_jquery.js";s:4:"0a65";s:16:"static/setup.txt";s:4:"e438";}',
+	'suggests' => array(
+	),
 );
 
 ?>
